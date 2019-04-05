b9a391c905d3382ab03158ec536421095049b8ae
Jonathan Dyason <jonathandyason@hotmail.com>
2019-04-05 21:05:24 +0200

    d

--- resources/views/my-family/04.php
+++ resources/views/my-family/04.php
@@ -74,7 +74,7 @@
 
 
     <script
-            src="http://code.jquery.com/jquery-3.3.1.min.js"
+            src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"
             integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
             crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
